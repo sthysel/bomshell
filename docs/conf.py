2d3cdab0c5ea4a43a9bb5de79cecf21f7e13c951
Thys Meintjes <sthysel@gmail.com>
2017-04-23 18:36:09 +0800

    Bump version: 1.0.0 → 1.0.1

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ project = 'bomshell'
 year = '2017'
 author = 'sthysel'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '1.0.0'
+version = release = '1.0.1'
 
 pygments_style = 'trac'
 templates_path = ['.']
